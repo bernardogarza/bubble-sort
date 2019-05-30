a857ea9df66319c55aa2aa1780779779210b6e4c
Bernardo Garza <bernardo.g.landa@gmail.com>
2019-05-30 15:40:22 -0500

    started with the project

---
+++ bubble-sort.rb
@@ -0,0 +1,17 @@
+def bubble_sort (arr)
+    count = 0
+    #aux = []
+    while arr.length-1>count
+        if arr[count] > arr[count+1]
+            arr[count], arr[count+1] = arr[count+1], arr[count]
+            count+=1
+            count-=1
+        else
+            count+=1
+        end
+    end
+    arr
+end
+arr=[1, 10, 3, 25, 2]
+print bubble_sort(arr)
+
