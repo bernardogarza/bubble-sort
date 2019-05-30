93b60033db4f66790a96aa3b6011495ecd11f326
Kalib Hackin <nagui08@hotmail.com>
2019-05-30 16:11:14 -0500

    Finished the first method and trying to understand what to do in the second method

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,11 +1,9 @@
 def bubble_sort (arr)
     count = 0
-    #aux = []
-    while arr.length-1>gjtcount
+    while arr.length-1>count
         if arr[count] > arr[count+1]
             arr[count], arr[count+1] = arr[count+1], arr[count]
-            count+=1
-            count-=1
+            count = 0
         else
             count+=1
         end
@@ -13,5 +11,13 @@ def bubble_sort (arr)
     arr
 end
 arr=[1, 10, 3, 25, 2]
-print bubble_sort(arr)
+# print bubble_sort(arr)
 
+def bubble_sort_by (arr)
+    abc = yield(arr)
+    puts abc
+end
+
+arr2 = ["hi","hello","hey"]
+
+bubble_sort_by(arr2) { |left, right| left.length - right.length }
